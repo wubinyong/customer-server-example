9b3adb7a9fbfd47bf18accbaafeb45deb386fd77
xwnor <wangxin90@outlook.com>
2021-06-04 23:10:25 +0200

    Add access log

--- server-example/index.js
+++ server-example/index.js
@@ -2,8 +2,9 @@
 
 const https = require('https');
 const express = require('express');
-const Logger = require('logger');
 const fs = require('fs');
+const morgan = require('morgan');
+const path = require('path');
 const app = express();
 const certificates_handler = require('./certificate/app.js');
 
@@ -11,6 +12,11 @@ const ca = fs.readFileSync('./cert_ca/ca.crt');
 const cert = fs.readFileSync('./cert_server/cert.crt');
 const key = fs.readFileSync('./cert_server/cert.key');
 
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }));
 app.use('/', certificates_handler);
 
 let options = {
